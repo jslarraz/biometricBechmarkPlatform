a0c3f4d88e5ba86b899a9c845f01c1089444440a
jslarraz <jslarraz@gmail.com>
2021-05-12 17:56:03 +0200

    {"preprocessing_san18": {"multicycles_san18": {"manhattan_san18": {"nonin": {"d2": 19.545088566827697, "d3": 19.233091787439612}, "berry": {"d2": 22.19202898550725, "d3": 19.283413848631238}}}}}

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,13 +1,14 @@
 from system import *
+import json
 
 eer = {}
 for preprocessing in [preprocessing_san18]:
     eer[preprocessing.__name__] = {}
 
-    for feature_extractor in [mean_cycle_san18, multicycles_san18]:
+    for feature_extractor in [multicycles_san18]:
         eer[preprocessing.__name__][feature_extractor.__name__] = {}
 
-        for classifier in [manhattan_san18, euclidean_san18]:
+        for classifier in [manhattan_san18]:
             eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__] = {}
 
             # System definition
@@ -19,18 +20,17 @@ for preprocessing in [preprocessing_san18]:
                 for ts in ['d2', 'd3']:
                     eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts] = []
 
-                    for enrollment_gap in [0,5,10]:
-                        s.enrollment(from_file('ddbb', device, 'd1', 10+enrollment_gap, 40+enrollment_gap))
+                    for enrollment_gap in [0,15,30]:
+                        s.enrollment(from_file('ddbb', device, 'd1', enrollment_gap, 30+enrollment_gap))
 
-                        for testing_gap in [0,5,10]:
-                            th, far, frr, eer_aux = s.testing(from_file('ddbb', device, ts, 10+testing_gap, 40+testing_gap))
+                        for testing_gap in [0,15,30]:
+                            th, far, frr, eer_aux = s.testing(from_file('ddbb', device, ts, testing_gap, 30+testing_gap))
                             eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts].append(eer_aux)
 
                     eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts] = mean(eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts])
                     print(mean(eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts]))
 print(eer)
-
-
+print(json.dumps(eer))
 
 
 
