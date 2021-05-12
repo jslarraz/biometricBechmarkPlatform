9fd60a35f9fc82294c2fbad254fb88e2d2819d93
jslarraz <jslarraz@gmail.com>
2021-05-12 18:49:19 +0200

    empiezan las pruebas

--- tests/test_run.py
+++ tests/test_run.py
@@ -20,17 +20,17 @@ for preprocessing in [preprocessing_san18]:
                 for ts in ['d2', 'd3']:
                     eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts] = []
 
-                    for enrollment_gap in [0,15,30]:
-                        s.enrollment(from_file('ddbb', device, 'd1', enrollment_gap, 30+enrollment_gap))
+                    for enrollment_gap in [0,10,20]:
+                        s.enrollment(from_file('ddbb', device, 'd1', enrollment_gap, 35+enrollment_gap))
 
-                        for testing_gap in [0,15,30]:
-                            th, far, frr, eer_aux = s.testing(from_file('ddbb', device, ts, testing_gap, 30+testing_gap))
+                        for testing_gap in [0,10,20]:
+                            th, far, frr, eer_aux = s.testing(from_file('ddbb', device, ts, testing_gap, 35+testing_gap))
                             eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts].append(eer_aux)
 
                     eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts] = mean(eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts])
                     print(mean(eer[preprocessing.__name__][feature_extractor.__name__][classifier.__name__][device][ts]))
-print(eer)
-print(json.dumps(eer))
+
+print(json.dumps(eer, indent=2))
 
 
 
